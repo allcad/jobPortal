d44eebba4929fba2492240efc9fe7ad6c01b7854
komal <komal@allcad.com>
2017-10-26 22:42:21 +0530

    work on recruiter profile

--- src/app/recruiter-manage-profile/recruiter-manage-profile.component.ts
+++ src/app/recruiter-manage-profile/recruiter-manage-profile.component.ts
@@ -41,10 +41,32 @@ export class RecruiterManageProfileComponent implements OnInit {
 responseData;
 succesMessageFlag=false;
 ErrorMesageFlag=false;
-  constructor(public _commonRequestService: CommonRequestService) { }
+showMultipleAddress = false;
+//list = [];
+//i = 0;
+addMulAddArray = [{'addresslLine1': 'line1', 'addressLine2': 'line2', 'city': 'city', 'country': 'cou', 'postCode': '12', 'telephone': '134'}]
+  constructor(public _commonRequestService: CommonRequestService) {
+    this.addMulAddArray.splice(0,1);
+   }
 
   ngOnInit() {
     this.getProfileDta()
+  }
+
+  addMultiplePhone() {
+    // this.i++;
+    // this.list.push(this.i);
+    // console.log("this.list--", this.list);
+    var newItem = 0;
+    this.addMulAddArray.push({'addresslLine1': '', 'addressLine2': '', 'city': '', 'country': '', 'postCode': '', 'telephone': ''})
+    newItem + 1;
+    this.showMultipleAddress = true;
+  }
+
+  removeAddress(index) {
+    console.log("this.addMulAddArray", this.addMulAddArray)
+    // var indexV = this.list.indexOf(index);
+    // this.list.splice(indexV, 1);
   }
 
    recruiterFileChangeEvent(fileInput: any) {
@@ -143,38 +165,38 @@ getProfileDta(){
    this.profileData =  this._commonRequestService.getDataWithoutObserval('recruiter-profile-view-data');
    console.log("this.pro--", this.profileData);
    if(this.profileData) {
-  this.companyName =this.profileData['companyDetails'].companyName;
-  this.companySize =this.profileData['companyDetails'].companySize;
-  this.addressName =this.profileData['companyDetails'].addressName;
-  this.addressLine1 =this.profileData['companyDetails'].addressLine1;
-  this.addressLine2 =this.profileData['companyDetails'].addressLine2;
-  this.city =this.profileData['companyDetails'].city;
-  this.country =this.profileData['companyDetails'].country;
-  this.postCode =this.profileData['companyDetails'].postCode;
-  this.telephone =this.profileData['companyDetails'].telephone;
-  this.postalAddressLine1 =this.profileData['companyDetails'].postalAddressLine1;
-  this.postalAddressLine2 =this.profileData['companyDetails'].postalAddressLine2;
-  this.postalCity =this.profileData['companyDetails'].postalCity;
+  this.companyName =this.profileData['companyDetails'] && this.profileData['companyDetails'].companyName ? this.profileData['companyDetails'].companyName : "";
+  this.companySize =this.profileData['companyDetails'] && this.profileData['companyDetails'].companySize ? this.profileData['companyDetails'].companySize : "";
+  this.addressName =this.profileData['companyDetails'] && this.profileData['companyDetails'].addressName ? this.profileData['companyDetails'].addressName : "";
+  this.addressLine1 =this.profileData['companyDetails'] && this.profileData['companyDetails'].addressLine1 ? this.profileData['companyDetails'].addressLine1 : "";
+  this.addressLine2 =this.profileData['companyDetails'] && this.profileData['companyDetails'].addressLine2 ? this.profileData['companyDetails'].addressLine2 : "";
+  this.city =this.profileData['companyDetails'] && this.profileData['companyDetails'].city ? this.profileData['companyDetails'].city : "";
+  this.country =this.profileData['companyDetails'] && this.profileData['companyDetails'].country ? this.profileData['companyDetails'].country : "";
+  this.postCode =this.profileData['companyDetails'] && this.profileData['companyDetails'].postCode ? this.profileData['companyDetails'].postCode : "";
+  this.telephone =this.profileData['companyDetails'] && this.profileData['companyDetails'].telephone ? this.profileData['companyDetails'].telephone : "";
+  this.postalAddressLine1 =this.profileData['companyDetails'] && this.profileData['companyDetails'].postalAddressLine1 ? this.profileData['companyDetails'].postalAddressLine1 : "";
+  this.postalAddressLine2 =this.profileData['companyDetails'] && this.profileData['companyDetails'].postalAddressLine2 ? this.profileData['companyDetails'].postalAddressLine2 : "";
+  this.postalCity =this.profileData['companyDetails'] && this.profileData['companyDetails'].postalCity ? this.profileData['companyDetails'].postalCity : "";
 
-  this.postalCountry =this.profileData['companyDetails'].postalCountry;
-  this.postalPostCode =this.profileData['companyDetails'].postalPostCode;
-  this.postalTelephone =this.profileData['companyDetails'].postalTelephone;
-  this.fileArray =this.profileData['companyDetails'].fileArray;
+  this.postalCountry =this.profileData['companyDetails'] && this.profileData['companyDetails'].postalCountry ? this.profileData['companyDetails'].postalCountry : "";
+  this.postalPostCode =this.profileData['companyDetails'] && this.profileData['companyDetails'].postalPostCode ? this.profileData['companyDetails'].postalPostCode : "";
+  this.postalTelephone =this.profileData['companyDetails'] && this.profileData['companyDetails'].postalTelephone ? this.profileData['companyDetails'].postalTelephone : ""; 
+  this.fileArray =this.profileData['companyDetails'] && this.profileData['companyDetails'].fileArray ? this.profileData['companyDetails'].fileArray : "";
   this.companyDescription= this.profileData.companyDescription;
-  this.webAddress= this.profileData['companySocial'].webAddress;
-  this.emailAddress= this.profileData['companySocial'].emailAddress;
+  this.webAddress= this.profileData['companySocial'] && this.profileData['companySocial'].webAddress ? this.profileData['companySocial'].webAddress : "";
+  this.emailAddress= this.profileData['companySocial'] && this.profileData['companySocial'].emailAddress ? this.profileData['companySocial'].emailAddress : "";
 
-  this.rssUrl= this.profileData['companySocial'].rssData.rssUrl;
-  this.fullUrlRssFeed= this.profileData['companySocial'].rssData.fullUrlRssFeed;
-  this.rssDisplayFeed= this.profileData['companySocial'].rssData.rssDisplayFeed;
+  this.rssUrl= this.profileData['companySocial'] && this.profileData['companySocial'].rssData && this.profileData['companySocial'].rssData.rssUrl ? this.profileData['companySocial'].rssData.rssUrl : "";
+  this.fullUrlRssFeed= this.profileData['companySocial'] && this.profileData['companySocial'].rssData && this.profileData['companySocial'].rssData.fullUrlRssFeed ? this.profileData['companySocial'].rssData.fullUrlRssFeed : "";
+  this.rssDisplayFeed= this.profileData['companySocial'] && this.profileData['companySocial'].rssData && this.profileData['companySocial'].rssData.rssDisplayFeed ? this.profileData['companySocial'].rssData.rssDisplayFeed : "";
 
-  this.socialLinkName= this.profileData['companySocial'].twitterData.socialLinkName;
-  this.fullUrlTwitterFeed= this.profileData['companySocial'].twitterData.fullUrlTwitterFeed;
-  this.twitterDisplayFeed= this.profileData['companySocial'].twitterData.twitterDisplayFeed;
+  this.socialLinkName= this.profileData['companySocial'] && this.profileData['companySocial'].twitterData && this.profileData['companySocial'].twitterData.socialLinkName ? this.profileData['companySocial'].twitterData.socialLinkName : "";
+  this.fullUrlTwitterFeed= this.profileData['companySocial'] && this.profileData['companySocial'].twitterData && this.profileData['companySocial'].twitterData.fullUrlTwitterFeed ? this.profileData['companySocial'].twitterData.fullUrlTwitterFeed : "";
+  this.twitterDisplayFeed= this.profileData['companySocial'] && this.profileData['companySocial'].twitterData && this.profileData['companySocial'].twitterData.twitterDisplayFeed ? this.profileData['companySocial'].twitterData.twitterDisplayFeed : "";
 
-  this.linkedinUrl= this.profileData['companySocial'].linkedinData.linkedinUrl;
-  this.fullUrlLinkedinFeed= this.profileData['companySocial'].linkedinData.fullUrlLinkedinFeed;
-  this.linkedinDisplayFeed= this.profileData['companySocial'].linkedinData.linkedinDisplayFeed;
+  this.linkedinUrl= this.profileData['companySocial'] && this.profileData['companySocial'].linkedinData && this.profileData['companySocial'].linkedinData.linkedinUrl ? this.profileData['companySocial'].linkedinData.linkedinUrl : "";
+  this.fullUrlLinkedinFeed= this.profileData['companySocial'] && this.profileData['companySocial'].linkedinData && this.profileData['companySocial'].linkedinData.fullUrlLinkedinFeed ? this.profileData['companySocial'].linkedinData.fullUrlLinkedinFeed : "";
+  this.linkedinDisplayFeed= this.profileData['companySocial'] && this.profileData['companySocial'].linkedinData && this.profileData['companySocial'].linkedinData.linkedinDisplayFeed ? this.profileData['companySocial'].linkedinData.linkedinDisplayFeed : "";
 }
 
 // this.profileData.
