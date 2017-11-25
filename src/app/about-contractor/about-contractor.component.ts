761c17ad54c80685b929dcc3933134b174e6064d
allcad <allcad@gmail.com>
2017-11-25 15:41:50 +0530

    add animation functionality

--- src/app/about-contractor/about-contractor.component.ts
+++ src/app/about-contractor/about-contractor.component.ts
@@ -10,6 +10,52 @@ export class AboutContractorComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
+
+  	$(document).on('scroll', function() {
+      let currentHeight = $(this).height();
+    if($('#contractorroket') && $('#contractorroket').position() && $('#contractorroket').position().top && $(this).scrollTop()>=$('#contractorroket').position().top){
+        $("#contractorroket").animate({top: '50px'},'slow',function(){});
+    }
+    var isAnimating = $('#contractorroketarrow').is(':animated');
+    console.log('isAnimating--', isAnimating, $('#contractorroketarrow').css('top'));
+    if ($('#contractorroketarrow').css('top') && parseInt($('#contractorroketarrow').css('top').split("px")[0]) <= 80)      {
+    if($('#contractorroketarrow') && $('#contractorroketarrow').position()){
+        $("#contractorroketarrow").animate({right: '+=10', top: '+=10'},'fast',function(){    
+          console.log($(this).css('top'));
+          if ($('#contractorroketarrow').css('top') && parseInt($('#contractorroketarrow').css('top').split("px")[0]) >= 80)    	{
+             $(this).stop(true,true);
+          }
+        });
+    }
+
+    }
+    // var isAnimating = $('.contractorroketarrow').is(':animated');
+    // if (!isAnimating && $('.contractorroketarrow').css('top') <= '80px') {    	
+    // if($('.contractorroketarrow') && $('.contractorroketarrow').position()){
+    //     $(".contractorroketarrow").animate({right: '+=10', top: '+=10'},'slow',function(){    
+    //       console.log($(this).css('top'));
+    //       if ($(this).css('top') > '80px')    	{
+    //          $(this).stop(true,true);
+    //       }
+    //     });
+    // }
+
+
+    // }
+    // var pTop = parseInt($('.contractorroketarrow').css('top'));
+    //     console.log("pTop--", pTop);
+    //     if (pTop === 80) {
+    //       $(".contractorroketarrow").stop(true,true);
+    //     }
+    //console.log( parseInt($(this).scrollTop().toString()) , parseInt(($(document).height() - $(window).height() - $(".contractorroketarrow").height()).toString()));
+    // if ( parseInt($(this).scrollTop().toString()) == parseInt(($(document).height() - $(window).height()).toString())) {
+    //   $(".contractorroketarrow").stop(true,true);
+    // }
+    // if($(".footer_bottom")) {
+    // 	$(".contractorroketarrow").stop(true,true);
+    // 	// alert("in");
+    // }
+})
   }
 
 }
--- src/app/about-recruiter/about-recruiter.component.ts
+++ src/app/about-recruiter/about-recruiter.component.ts
@@ -12,14 +12,34 @@ export class AboutRecruiterComponent implements OnInit {
 
   ngOnInit() {
   	 //$(".roket").animate({top: '-10px'});
+    
   	$(document).on('scroll', function() {
+      let currentHeight = $(this).height();
     if($('.roket') && $('.roket').position() && $('.roket').position().top && $(this).scrollTop()>=$('.roket').position().top){
-        $(".roket").animate({top: '0px'},'slow',function(){});
+        $(".roket").animate({top: '55px'},'slow',function(){});
     }
-    if($('.roket_arrow') && $('.roket_arrow').position() ){
-        $(".roket_arrow").animate({right: '+=10', top: '+=10'},'slow',function(){        	
+    var isAnimating = $('.roket_arrow').is(':animated');
+    console.log('isAnimating--', isAnimating, $('.roket_arrow').css('top'));
+    if ($('.roket_arrow').css('top') && parseInt($('.roket_arrow').css('top').split("px")[0]) <= 190)      {
+    if($('.roket_arrow') && $('.roket_arrow').position()){
+        $(".roket_arrow").animate({right: '+=10', top: '+=10'},'fast',function(){    
+          console.log($(this).css('top'));
+          if ($('.roket_arrow').css('top') && parseInt($('.roket_arrow').css('top').split("px")[0]) >= 190)    	{
+             $(this).stop(true,true);
+          }
         });
     }
+
+    }
+    // var pTop = parseInt($('.roket_arrow').css('top'));
+    //     console.log("pTop--", pTop);
+    //     if (pTop === 80) {
+    //       $(".roket_arrow").stop(true,true);
+    //     }
+    //console.log( parseInt($(this).scrollTop().toString()) , parseInt(($(document).height() - $(window).height() - $(".roket_arrow").height()).toString()));
+    // if ( parseInt($(this).scrollTop().toString()) == parseInt(($(document).height() - $(window).height()).toString())) {
+    //   $(".roket_arrow").stop(true,true);
+    // }
     // if($(".footer_bottom")) {
     // 	$(".roket_arrow").stop(true,true);
     // 	// alert("in");
