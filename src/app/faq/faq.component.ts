37d13a56316e911098ea765ca5c1de74b7b67a5c
allcad <allcad>
2017-12-12 19:35:54 +0530

    resolve issue

--- src/app/faq/faq.component.ts
+++ src/app/faq/faq.component.ts
@@ -12,4 +12,18 @@ export class FaqComponent implements OnInit {
   ngOnInit() {
   }
 
+  toggleAccordian(id) {
+  	if ($("#"+id).hasClass('in')) {
+  		//$("#"+id).removeClass('in');
+  		if($("#"+id).parent().find('span').hasClass('glyphicon-minus')) {
+  			$("#"+id).parent().find('span').removeClass('glyphicon-minus');
+  			$("#"+id).parent().find('span').addClass('glyphicon-plus');
+  			$("#"+id).removeClass('in');
+  		} else {
+  			$("#"+id).parent().find('span').removeClass('glyphicon-plus');
+  			$("#"+id).parent().find('span').addClass('glyphicon-minus')
+  		}
+  	} 
+  }
+
 }
