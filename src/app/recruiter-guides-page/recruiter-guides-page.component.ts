ce6b62703ccd18d0a49b2b0da6946b3cd6ad09b3
allcad <allcad>
2017-12-13 18:26:52 +0530

    change recruiter

--- src/app/recruiter-guides-page/recruiter-guides-page.component.ts
+++ src/app/recruiter-guides-page/recruiter-guides-page.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { CommonRequestService } from '../common-request.service';
 
 @Component({
   selector: 'app-recruiter-guides-page',
@@ -6,10 +7,35 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recruiter-guides-page.component.css']
 })
 export class RecruiterGuidesPageComponent implements OnInit {
-
-  constructor() { }
+ emailValue;
+ WSErrorMsg;
+  constructor(public _commonRequestService: CommonRequestService) { }
 
   ngOnInit() {
+  }
+
+   shareGuide() {
+   	this.WSErrorMsg = "";
+   	window.scroll(0,0);
+     var input = {
+     "send_to": this.emailValue
+   };
+   console.log("input--", input);
+   if(this.emailValue) {
+	   var wsUrl="http://dev.contractrecruit.co.uk/contractor_admin/api/post/share_guide";
+	       this._commonRequestService.postData(wsUrl,input).subscribe(
+	        data => {
+	          console.log("securityClearanceArray--", data);
+	          if(data && data.status == 'TRUE') {
+	          	this.WSErrorMsg = "";
+	            this.emailValue = "";
+	          } else {
+	          	this.WSErrorMsg = typeof (data.error) == 'object' ? data.error[0] : data.error;
+	          }
+	          //this.recruiterNameArray = data.data;
+	        }
+	    );
+	}
   }
 
 }
