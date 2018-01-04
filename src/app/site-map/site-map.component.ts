35f4b4dd537f170ab865c8d3dddccc889b6a6660
allcad <allcad>
2018-01-04 11:40:21 +0530

    work on site map

--- src/app/site-map/site-map.component.ts
+++ src/app/site-map/site-map.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { CommonRequestService } from '../common-request.service';
 
 @Component({
   selector: 'app-site-map',
@@ -6,10 +7,30 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./site-map.component.css']
 })
 export class SiteMapComponent implements OnInit {
-
-  constructor() { }
+  menuList;
+  constructor(private _commonRequestService: CommonRequestService) { }
 
   ngOnInit() {
+    this.getMenuData();
+  }
+
+  getMenuData() {
+    let url = "http://dev.contractrecruit.co.uk/contractor_admin/api/get/staticpages/menu";
+    this._commonRequestService.getData(url).subscribe(
+      data => {
+        this.menuList = data.data;
+        console.log("this.menuList", this.menuList);
+        for(let i=0; i<this.menuList.length; i++){
+          if(this.menuList[i].manu_label == 'IT Contractors'){
+            this.menuList[i].subpages.unshift({"manu_label" : "Join Us on ContractRecruit", "pages": [{"manu_label" : "Login", "menu_link": "public/contractorLogin"}, {"manu_label" : "Sign Up", "menu_link": "public/contractorSignup"}]});
+          }else if(this.menuList[i].manu_label == 'IT Recruiters'){
+            this.menuList[i].subpages.unshift({"manu_label" : "Join Us on ContractRecruit", "pages": [{"manu_label" : "Login", "menu_link": "public/recruiterLogin"}, {"manu_label" : "Sign Up", "menu_link": "public/recruiterRegister"}, {"manu_label" : "Why Should I Sign Up", "menu_link": "public/about-recruiter"}]});
+          }
+        }
+
+        console.log("this.menuList", this.menuList)
+      }
+    );
   }
 
 }
